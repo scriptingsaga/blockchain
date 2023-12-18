2262664fbb0c4f1f602f747e6f12617ebfa484cc
scripting saga <scriptingsaga@gmail.com>
2023-12-18 14:30:25 -0600

    simple blockchain ran through k8s

--- simpleBlockchain.py
+++ simpleBlockchain.py
@@ -3,61 +3,75 @@ import hashlib
 import json
 from time import time
 
-class simpleBlockchain:
+class Blockchain:
     def __init__(self):
         self.chain = []
-        self.currentTransactions = []
-        self.newBlock(previousHash='1', proof=100)
+        self.current_transactions = []
+        self.new_block(previous_hash='1', proof=100)
 
-    def newBlock(self, proof, previousHash=None):
+    def new_block(self, proof, previous_hash=None):
         block = {
-            'index':len(self.chain)+1,
+            'index': len(self.chain) + 1,
             'timestamp': time(),
-            'transactions': self.currentTransactions,
+            'transactions': self.current_transactions,
             'proof': proof,
-            'previousHash': previousHash or 
-                self.hash(self.chain[-1]) if self.chain else None,
+            'previous_hash': previous_hash or self.hash(self.chain[-1]) if self.chain else None,
         }
-        self.currentTransactions = []
+        self.current_transactions = []
         self.chain.append(block)
         return block
-    
-    def newTransaction(self,sender, recipient, amount):
-        self.currentTransactions.append({
-            'sender':sender,
-            'recipient':recipient,
+
+    def new_transaction(self, sender, recipient, amount):
+        self.current_transactions.append({
+            'sender': sender,
+            'recipient': recipient,
             'amount': amount,
         })
-        return self.lastBlock['index']+1
-    
+        return self.last_block['index'] + 1
+
     @staticmethod
     def hash(block):
-        blockString = json.dumps(block,sort_keys=True).encode()
-        return hashlib.sha256(blockString).hexdigest()
-        
-app = Flask(__name__)
-blockchain = simpleBlockchain()
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
 
-@app.route('/mining', methods=['GET'])
-def mining():
+    @property
+    def last_block(self):
+        return self.chain[-1]
+
+app = Flask(__name__)
+blockchain = Blockchain()
+
+@app.route('/mine', methods=['GET'])
+def mine():
+    # Mining logic here, for simplicity, we'll just add a new block with proof 1
     proof = 1
-    previousHash = simpleBlockchain.hash(simpleBlockchain.last_block)
-    simpleBlockchain.newTransaction(sender="0",recipient="address1", amount=1)
-    block = simpleBlockchain.newBlock(proof, previousHash)
+    previous_hash = blockchain.hash(blockchain.last_block)
+    blockchain.new_transaction(sender="0", recipient="your_address", amount=1)
+    block = blockchain.new_block(proof, previous_hash)
     response = {
-        'message': "new block forged",
+        'message': "New Block Forged",
         'index': block['index'],
         'transactions': block['transactions'],
         'proof': block['proof'],
-        'previousHash': block['previousHash'],
+        'previous_hash': block['previous_hash'],
     }
-    return jsonify(response),200
+    return jsonify(response), 200
+
+@app.route('/transactions/new', methods=['POST'])
+def new_transaction():
+    values = request.get_json()
+    required = ['sender', 'recipient', 'amount']
+    if not all(k in values for k in required):
+        return 'Missing values', 400
+    index = blockchain.new_transaction(values['sender'], values['recipient'], values['amount'])
+    response = {'message': f'Transaction will be added to Block {index}'}
+    return jsonify(response), 201
 
 @app.route('/chain', methods=['GET'])
-def fullChain():
-    response={
-        'chain': simpleBlockchain.chain,
-        'length': len(simpleBlockchain.chain),
+def full_chain():
+    response = {
+        'chain': blockchain.chain,
+        'length': len(blockchain.chain),
     }
     return jsonify(response), 200
 
